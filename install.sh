fd1a04071bdf501f2e44569af0e8dd40c36419d8
SHAHANASSHA <126233992+SHAHANASSHA@users.noreply.github.com>
2025-06-26 10:18:03 +0530

    major: Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 go build . 
+sudo systemctl stop bender || true
 
 sudo mkdir -p /usr/local/bin 
 
@@ -10,14 +11,14 @@ sudo chmod +x /usr/local/bin/bender
 
 service="[Unit]
 Description=Bender service
-After=network.target
+After=network.target redis.service
  
 [Service] 
 Type=simple
 ExecStart=/usr/local/bin/bender
 Restart=on-failure
 User=$(whoami)
-WorkingDirectory=/home/$(whoami)
+WorkingDirectory=/usr/local/bin
 SupplementaryGroups=docker
 
 [Install]
@@ -27,7 +28,7 @@ echo "$service" | sudo tee /etc/systemd/system/bender.service > /dev/null
 
 sudo chown root:docker /var/run/docker.sock
 sudo chmod 0660 /var/run/docker.sock
-
+sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 
 sudo systemctl enable bender
